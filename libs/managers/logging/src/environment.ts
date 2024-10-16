fcfcaee96249614d0b6ccf36768c58e55a78ddd8
ZNackasha <zaidnackasha22@gmail.com>
2024-10-16 18:18:42 -0400

    Update environment.ts

--- libs/managers/logging/src/environment.ts
+++ libs/managers/logging/src/environment.ts
@@ -1,7 +1,7 @@
 import { Verbosity } from './entities';
 
 export class Environment {
-  public static getName = () => 'zsoft-utils-logging';
+  public static getName = () => 'benzinga-logging';
   public static getEnvironment = (env: Record<string, string>) => ({
     verbosity: (env["verbosity"] ?? 'info') as Verbosity,
   });
