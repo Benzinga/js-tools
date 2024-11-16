bc67616339d68f3284eac806db56029e02a25f77
ZNackasha <zaidnackasha22@gmail.com>
2024-11-16 00:54:29 -0500

    Simplify exports in index.ts by consolidating module exports for better maintainability

--- libs/vanilla-utils/subscribable/src/index.ts
+++ libs/vanilla-utils/subscribable/src/index.ts
@@ -1,33 +1,4 @@
-import {
-  ExtendedSubscribable,
-  ExtendedListenableSubscribable,
-  ListenableSubscribable,
-  Subscribable,
-  SubscribableEvent,
-  SubscribableEventType,
-  SubscriberId,
-  Subscription,
-  SubscriptionType,
-  SubscriptionExtendedType,
-} from './subscribable';
-import { SubscribableMultiplexer, SubscribableMultiplexerEvent } from './multiplexer';
-import { SubscribableHolder } from './holder';
-
-export {
-  Subscribable,
-  SubscribableHolder,
-  ExtendedSubscribable,
-  ExtendedListenableSubscribable,
-  ListenableSubscribable,
-  SubscribableMultiplexer,
-};
-
-export type {
-  SubscribableEvent,
-  SubscriberId,
-  SubscribableMultiplexerEvent,
-  SubscribableEventType,
-  Subscription,
-  SubscriptionType,
-  SubscriptionExtendedType,
-};
+export * from './subscribable';
+export * from './holder';
+export * from './multiplexer';
+export * from './subscriptionFieldTracker';
